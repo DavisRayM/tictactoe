4d00006a7df32fcedd022d80266f0481ebe28ff1
Davis Muro <git@davisraym.com>
2025-04-02 16:40:16 -0700

    feat(test): add tests for `make_move` `update_state` and `free_board`
    - cleanup(test): rename the prefix `test_board` to `test`; It's pretty
      evident that we're testing the board from the file name.

--- tests/test_board.h
+++ tests/test_board.h
@@ -1,5 +1,5 @@
-#ifndef _TICTACTOE_TEST_BOARD_H
-#define _TICTACTOE_TEST_BOARD_H 1
+#ifndef _TICTACTOE_test_H
+#define _TICTACTOE_test_H 1
 #include "../src/board.h"
 #include <CUnit/CUnit.h>
 #include <stdbool.h>
@@ -28,17 +28,23 @@ int clean_boardsuite(void) {
   }
 }
 
-void test_board_init(void) {
+void test_init(void) {
   struct Board board;
   initialize_board(&board);
 
   for (int i = 0; i < BOARDSIZE; i++)
     CU_ASSERT(EMPTY == board.state[i]);
 
+  CU_ASSERT(board.state != NULL);
+  CU_ASSERT(board.gen == 1);
+
   free_board(&board);
+
+  CU_ASSERT(board.state == NULL);
+  CU_ASSERT(board.gen == 0);
 }
 
-void test_board_currentplayer(void) {
+void test_currentplayer(void) {
   struct Board board;
   initialize_board(&board);
 
@@ -58,7 +64,7 @@ void test_player_token(void) {
   CU_ASSERT('X' == player_token(X));
 }
 
-void test_board_valid_moves(void) {
+void test_valid_moves(void) {
   struct Board board;
   struct PlayerMove *moves = NULL;
   int numMoves = 0;
@@ -104,15 +110,101 @@ void test_board_valid_moves(void) {
   free_board(&board);
 }
 
+void test_game_winner() {
+  struct Board board;
+  enum TokenType row_winner[] = {
+      EMPTY, X, EMPTY, CIRCLE, CIRCLE, CIRCLE, X, X, EMPTY,
+  };
+  enum TokenType col_winner[] = {X, CIRCLE, X, X, X, X, CIRCLE, CIRCLE, X};
+  enum TokenType diagonal_winner1[] = {
+      EMPTY, X, X, CIRCLE, X, CIRCLE, X, X, EMPTY,
+  };
+  enum TokenType diagonal_winner2[] = {
+      CIRCLE, X, CIRCLE, CIRCLE, CIRCLE, X, X, X, CIRCLE,
+  };
+  enum TokenType no_winner[] = {
+      CIRCLE, X, CIRCLE, CIRCLE, X, X, X, CIRCLE, CIRCLE,
+  };
+
+  initialize_board(&board);
+
+  CU_ASSERT(EMPTY == game_winner(board.state));
+
+  free(update_state(&board, row_winner));
+  CU_ASSERT(CIRCLE == game_winner(board.state));
+
+  update_state(&board, col_winner);
+  CU_ASSERT(X == game_winner(board.state));
+
+  update_state(&board, diagonal_winner1);
+  CU_ASSERT(X == game_winner(board.state));
+
+  update_state(&board, diagonal_winner2);
+  CU_ASSERT(CIRCLE == game_winner(board.state));
+
+  update_state(&board, no_winner);
+  CU_ASSERT(EMPTY == game_winner(board.state));
+}
+
+void test_make_move() {
+  struct Board board;
+  struct PlayerMove *moves = NULL;
+  BoardState new_state = NULL;
+  enum TokenType player = EMPTY;
+  int numMoves = 0;
+  int idx = 0;
+
+  initialize_board(&board);
+  moves = valid_moves(board.state, &numMoves);
+
+  CU_ASSERT(numMoves != 0);
+
+  new_state = make_move(&board, moves[0]);
+  player = current_player(&board);
+  idx = moves[0].x * 3 + moves[0].y;
+
+  CU_ASSERT(player != board.state[idx]);
+  CU_ASSERT(player == new_state[idx]);
+
+  free(moves);
+  free(new_state);
+  free_board(&board);
+}
+
+void test_update_state() {
+  struct Board board;
+  enum TokenType new_state[] = {
+      CIRCLE, CIRCLE, CIRCLE, CIRCLE, CIRCLE, CIRCLE, CIRCLE, CIRCLE, CIRCLE,
+  };
+  initialize_board(&board);
+
+  CU_ASSERT(board.gen == 1);
+  for (int x = 0; x < 3; x++)
+    for (int y = 0; y < 3; y++)
+      CU_ASSERT(board.state[x * 3 + y] != new_state[x * 3 + y]);
+
+  free(update_state(&board, new_state));
+  CU_ASSERT(board.gen == 2);
+  for (int x = 0; x < 3; x++)
+    for (int y = 0; y < 3; y++)
+      CU_ASSERT(board.state[x * 3 + y] == new_state[x * 3 + y]);
+}
+
 int register_boardtests(CU_pSuite suite) {
-  if ((NULL == CU_add_test(suite, "test of board initialize_board()",
-                           test_board_init)) ||
+  if ((NULL ==
+       CU_add_test(suite, "test of board initialize_board()", test_init)) ||
       (NULL == CU_add_test(suite, "test of board current_player()",
-                           test_board_currentplayer)) ||
+                           test_currentplayer)) ||
       (NULL ==
        CU_add_test(suite, "test of board player_token()", test_player_token)) ||
-      (NULL == CU_add_test(suite, "test of board valid_moves()",
-                           test_board_valid_moves))) {
+      (NULL ==
+       CU_add_test(suite, "test of board valid_moves()", test_valid_moves)) ||
+      (NULL ==
+       CU_add_test(suite, "test of board game_winner()", test_game_winner)) ||
+      (NULL ==
+       CU_add_test(suite, "test of board make_move()", test_make_move)) ||
+      (NULL ==
+       CU_add_test(suite, "test of board update_state()", test_update_state))) {
     return -1;
   }
   return 0;
