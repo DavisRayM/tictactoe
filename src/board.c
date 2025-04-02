a585df5fedc3a19e87c154bfade8326f7e951ead
Davis Muro <git@davisraym.com>
2025-04-02 07:43:33 -0700

    feat(board): add `valid_moves` function

--- src/board.c
+++ src/board.c
@@ -14,6 +14,13 @@ void free_board(struct Board *board) {
   board->gen = 0;
 }
 
+struct PlayerMove create_move(int x, int y) {
+  struct PlayerMove move;
+  move.x = x;
+  move.y = y;
+  return move;
+}
+
 enum TokenType current_player(struct Board *board) {
   switch (board->gen % 2) {
   case 0:
@@ -26,9 +33,31 @@ enum TokenType current_player(struct Board *board) {
 
 enum TokenType is_game_over(struct Board *board) { return EMPTY; }
 
-struct PlayerMove *valid_moves(struct Board *board) { return NULL; }
+struct PlayerMove *valid_moves(struct Board *board, int *numMoves) {
+  *numMoves = 0;
+  for (int i = 0; i < BOARDSIZE; i++)
+    if (board->state[i] == EMPTY)
+      *numMoves += 1;
 
-BoardState make_move(struct Board *board, struct PlayerMove *move) {
+  if (*numMoves == 0)
+      return NULL;
+
+  struct PlayerMove *moves =
+      (struct PlayerMove *)malloc(sizeof(struct PlayerMove) * (*numMoves));
+
+  int i = 0;
+  int j = 0;
+  while (i < *numMoves && j < BOARDSIZE) {
+    if (board->state[j] == EMPTY) {
+      moves[i++] = create_move(j / 3, j % 3);
+    }
+    j++;
+  }
+
+  return moves;
+}
+
+BoardState make_move(struct Board *board, struct PlayerMove move) {
   return board->state;
 }
 
--- tests/test_board.h
+++ tests/test_board.h
@@ -2,9 +2,15 @@
 #define _TICTACTOE_TEST_BOARD_H 1
 #include "../src/board.h"
 #include <CUnit/CUnit.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static FILE *temp_file = NULL;
+
+static bool playermove_equal(struct PlayerMove a, struct PlayerMove b) {
+  return a.x == b.x && a.y == b.y;
+}
 
 int init_boardsuite(void) {
   if (NULL == (temp_file = fopen("board_test.txt", "w+")))
@@ -52,13 +58,61 @@ void test_player_token(void) {
   CU_ASSERT('X' == player_token(X));
 }
 
+void test_board_valid_moves(void) {
+  struct Board board;
+  struct PlayerMove *moves = NULL;
+  int numMoves = 0;
+  initialize_board(&board);
+
+  moves = valid_moves(&board, &numMoves);
+
+  CU_ASSERT(numMoves == BOARDSIZE);
+  CU_ASSERT(moves != NULL);
+
+  int i = 0;
+  for (int x = 0; x < 3; x++)
+    for (int y = 0; y < 3; y++)
+      CU_ASSERT_TRUE(playermove_equal(moves[i++], create_move(x, y)));
+
+  board.state[1] = CIRCLE;
+  free(moves);
+
+  moves = NULL;
+  moves = valid_moves(&board, &numMoves);
+
+  CU_ASSERT(numMoves == BOARDSIZE - 1);
+  CU_ASSERT(moves != NULL);
+
+  i = 0;
+  for (int x = 0; x < 3; x++)
+    for (int y = 0; y < 3; y++) {
+      if (i == 1)
+        y += 1;
+      CU_ASSERT_TRUE(playermove_equal(moves[i++], create_move(x, y)));
+    }
+
+  free(moves);
+  for (int i = 0; i < BOARDSIZE; i++)
+      board.state[i] = CIRCLE;
+
+  moves = NULL;
+  moves = valid_moves(&board, &numMoves);
+
+  CU_ASSERT(numMoves == 0);
+  CU_ASSERT(moves == NULL);
+
+  free_board(&board);
+}
+
 int register_boardtests(CU_pSuite suite) {
   if ((NULL == CU_add_test(suite, "test of board initialize_board()",
                            test_board_init)) ||
       (NULL == CU_add_test(suite, "test of board current_player()",
                            test_board_currentplayer)) ||
       (NULL ==
-       CU_add_test(suite, "test of board player_token()", test_player_token))) {
+       CU_add_test(suite, "test of board player_token()", test_player_token)) ||
+      (NULL == CU_add_test(suite, "test of board valid_moves()",
+                           test_board_valid_moves))) {
     return -1;
   }
   return 0;
