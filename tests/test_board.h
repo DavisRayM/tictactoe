a9d45e6132cff9c126a359db40cb826e19fe3c73
Davis Muro <git@davisraym.com>
2025-04-02 06:17:51 -0700

    test(board): add test for `current_player`

--- tests/test_board.h
+++ tests/test_board.h
@@ -1,8 +1,8 @@
 #ifndef _TICTACTOE_TEST_BOARD_H
 #define _TICTACTOE_TEST_BOARD_H 1
+#include "../src/board.h"
 #include <CUnit/CUnit.h>
 #include <stdio.h>
-#include "../src/board.h"
 
 static FILE *temp_file = NULL;
 
@@ -23,18 +23,37 @@ int clean_boardsuite(void) {
 }
 
 void test_board_init(void) {
-    struct Board board;
-    initialize_board(&board);
+  struct Board board;
+  initialize_board(&board);
 
-    for (int i = 0; i < BOARDSIZE; i++)
-        CU_ASSERT(EMPTY == board.state[i]);
+  for (int i = 0; i < BOARDSIZE; i++)
+    CU_ASSERT(EMPTY == board.state[i]);
+
+  free_board(&board);
+}
+
+void test_board_currentplayer(void) {
+  struct Board board;
+  initialize_board(&board);
+
+  for (int i = 0; i < 5; i++) {
+    CU_ASSERT(X == current_player(&board));
+    board.gen += 1;
+    CU_ASSERT(CIRCLE == current_player(&board));
+    board.gen += 1;
+  }
+
+  free_board(&board);
 }
 
 int register_boardtests(CU_pSuite suite) {
-    if (NULL == CU_add_test(suite, "test of board initializer", test_board_init)) {
-        return -1;
-    }
-    return 0;
+  if ((NULL ==
+       CU_add_test(suite, "test of board initializer", test_board_init)) ||
+      (NULL == CU_add_test(suite, "test of board current_player()",
+                           test_board_currentplayer))) {
+    return -1;
+  }
+  return 0;
 }
 
 #endif
